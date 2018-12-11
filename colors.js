097a3b75d217c5451da9ff963f2e68288775666e
SeonghwanKim13109 <reasonby13@naver.com>
2018-12-11 19:47:40 +0900

    Jquery 사용하기

--- colors.js
+++ colors.js
@@ -1,24 +1,30 @@
 var Body = {
   setColor : function(value){
-    var target = document.querySelector('body');
+    // var target = document.querySelector('body');
+
     if(value === 'ON'){
-      target.style.backgroundColor='black';
-      target.style.color='white';
+        $('body').css('backgroundColor','black');
+        $('body').css('color','white');
+      // target.style.backgroundColor='black';
+      // target.style.color='white';
     }
     else{
-      target.style.backgroundColor='white';
-      target.style.color='black';
+      $('body').css('backgroundColor','white');
+      $('body').css('color','black');
+      // target.style.backgroundColor='white';
+      // target.style.color='black';
     }
   }
 }
 var Link = {
   setColor : function(color){
-    var link = document.querySelectorAll('a');
-    var i = 0;
-    while(i <link.length){
-      link[i].style.color = color;
-      i++;
-    }
+    // var link = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i <link.length){
+    //   link[i].style.color = color;
+    //   i++;
+    // }
+    $('a').css('color',color); //이 홈페이지에 있는 모든  a 태그를 제어하겠다는 의미
   }
 }
 
